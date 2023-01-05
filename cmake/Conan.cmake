2063cfbaea29b355d91891c5cda598e6acd87758
Maximilian Kleinert <kleinert.max@gmail.com>
2023-01-05 19:09:51 +0100

    Update Conan.cmake Create codeql.yml

--- cmake/Conan.cmake
+++ cmake/Conan.cmake
@@ -6,22 +6,16 @@ macro(run_conan)
     get_property(isMultiConfig GLOBAL PROPERTY GENERATOR_IS_MULTI_CONFIG)
     if (isMultiConfig)
         SET(CONAN_CMAKE_GENERATOR cmake_find_package_multi)
-        SET(CONAN_CMAKE_FILE_URL "https://raw.githubusercontent.com/conan-io/cmake-conan/develop/conan.cmake")
-        # We cannot expect the hash of the development version
-        #SET(CONAN_CMAKE_FILE_HASH 855c95e546dcaf0538369e8a6dfa37e219cb9be2f9523eb2119755ebee8eef4f)
     else ()
         SET(CONAN_CMAKE_GENERATOR cmake_find_package)
-        SET(CONAN_CMAKE_FILE_URL "https://raw.githubusercontent.com/conan-io/cmake-conan/v0.16.1/conan.cmake")
-        SET(CONAN_CMAKE_FILE_HASH 396e16d0f5eabdc6a14afddbcfff62a54a7ee75c6da23f32f7a31bc85db23484)
     endif ()
 
-    if (NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
-        message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
-        file(DOWNLOAD "${CONAN_CMAKE_FILE_URL}"
-                "${CMAKE_BINARY_DIR}/conan.cmake"
-                EXPECTED_HASH SHA256=${CONAN_CMAKE_FILE_HASH}
-                TLS_VERIFY ON)
-    endif ()
+    if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
+      message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
+      file(DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/0.18.1/conan.cmake"
+                    "${CMAKE_BINARY_DIR}/conan.cmake"
+                    TLS_VERIFY ON)
+    endif()
 
     include(${CMAKE_BINARY_DIR}/conan.cmake)
 
